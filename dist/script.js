2aa40ac469c551c80bdf6b0bd6d563323d454797
Ravana <47528708+ravana69@users.noreply.github.com>
2022-07-22 10:04:07 +0530

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -201,7 +201,7 @@ let drawVisual = null
 const processAudio = mp3 => {
   let audio = null
   if(mp3 === 'default'){
-    audio = new Audio('https://s3-us-west-2.amazonaws.com/s.cdpn.io/1468070/Boost.mp3')
+    audio = new Audio('http://node-09.zeno.fm/r8d5gau35bhvv?zs=vVLi9nqAQ3qJgYO6lvta2g&rj-tok=AAABgiQ31pAAHbLYrrZsDUMKdQ&rj-ttl=5')
     audio.crossOrigin = 'anonymous'
   }
   else{
